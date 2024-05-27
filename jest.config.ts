3d49907b96c4f76751f1cae447078f74ba538988
grayarrow <grayarrow@gmail.com>
2024-05-27 04:44:21 -0400

    Trying to run tests as a module

--- jest.config.ts
+++ jest.config.ts
@@ -2,15 +2,19 @@ import type { Config } from 'jest'
 import { defaults } from 'jest-config'
 
 const config: Config = {
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'],
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'js'],
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts', '.mts'],
+  // setupFiles: ['<rootDir>/__test__/setupTests.js'],
+  setupFilesAfterEnv: ['<rootDir>/__tests__/setupAfterEnvGlobal.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+
   // collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/types/**/*.ts'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
+  // transform: {
+  //   '^.+\\.ts$': 'ts-jest',
+  // },
 }
 
 export default config
