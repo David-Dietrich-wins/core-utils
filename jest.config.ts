543737a2c6436b21d6bdfa3cc5b5cb6b2329c173
ddietrich <ddietrich@intecore.com>
2025-02-09 14:40:23 -0500

    unit tests now working

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,20 @@
 import type { Config } from 'jest'
-import { defaults } from 'jest-config'
+// import { defaults } from 'jest-config'
 
 const config: Config = {
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'js'],
-  modulePathIgnorePatterns: ['<rootDir>/dist/'],
-  preset: 'ts-jest',
+  // moduleFileExtensions: [...defaults.moduleFileExtensions, 'mjs'],
+  // modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  // preset: 'ts-jest',
   testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.ts', '.mts'],
+  // extensionsToTreatAsEsm: ['.ts', '.mts'],
   // setupFiles: ['<rootDir>/__test__/setupTests'],
-  setupFilesAfterEnv: ['<rootDir>/__tests__/setupAfterEnvGlobal.ts'],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/__tests__/setupAfterEnvGlobal.ts'],
+  // setupFilesAfterEnv: ['<rootDir>/__tests__/setupAfterEnvGlobal.ts'],
+  // testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/__tests__/setupAfterEnvGlobal.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['node_modules'],
+  testMatch: ['**/*.test.ts', '**/*.test.mts', '**/*.test.mjs'],
 
   // collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/types/**/*.ts'],
   // transform: {
